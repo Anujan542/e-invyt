902d89517ac68bbd7bad404597330c88f1688c54
anujan542 <anujan9300@gmail.com>
2025-08-19 14:22:40 +0530

    Removed unwated stuff

--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -1,6 +1,6 @@
 import { Menu, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { cn } from '@/lib/utils';
 import { useTheme } from '../theme-provider';
