fe53e35b9fff359a33d23c5a9a7225e0b7ad4300
anujan542 <anujan9300@gmail.com>
2025-08-23 11:55:33 +0530

    added string for mongoDB

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -3,7 +3,7 @@ import colors from "colors";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log("MongoDB Connected".bgCyan);
   } catch (error) {
     console.log("error connection to MongoDB: ".bgRed, error.message);
