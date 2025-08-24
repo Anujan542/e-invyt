2ce88108427afca4b1d7d055dc8fd3ec00b016e6
anujan542 <anujan9300@gmail.com>
2025-08-24 07:05:21 +0530

    Added trust proxy

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.set("trust proxy", 1);
+
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
